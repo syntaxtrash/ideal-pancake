dd6b584046769741e572300360c239a1e2fae105
Aaron Aco <aaronacodev@gmail.com>
2024-02-20 21:25:55 +0800

    add initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+var char = "Juan";
+console.log(char);
+var input = document.querySelectorAll("input");
+console.log(input);
+for (var index = 0; index < input.length; index++) {
+    var element = input[index];
+    console.log(element);
+}
---
+++ script.ts
@@ -0,0 +1,13 @@
+const char = "Juan";
+
+console.log(char);
+
+const input = document.querySelectorAll("input");
+
+console.log(input);
+
+for (let index = 0; index < input.length; index++) {
+    const element = input[index];
+    console.log(element);
+    
+}
