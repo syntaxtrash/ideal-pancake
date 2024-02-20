05949b439e517b7600db11104ce3297f3876203c
Aaron Aco <aaronacodev@gmail.com>
2024-02-21 04:45:56 +0800

    add tuples

--- public/app.js
+++ public/app.js
@@ -8,42 +8,16 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 const ul = document.querySelector("ul");
 const list = new ListTemplates(ul);
+let values;
+values = [tofrom.value, details.value, amount.valueAsNumber];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "start");
 });
-// GENERICS
-const addUID = (obj) => {
-    //   console.log("obj", obj);
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-// const addUID = <T extends { name: string }>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return { ...obj, uid };
-// };
-let docOne = addUID({ name: "pedro", age: 40 });
-// let docTwo = addUID("shaun");
-console.log(docOne.name);
-const docThree = {
-    uid: 1,
-    resourceName: "person",
-    data: { name: "shaun" },
-};
-const docFour = {
-    uid: 1,
-    resourceName: "shoppingList",
-    data: ["bread", "milk"],
-};
-console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -12,59 +12,18 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 const ul = document.querySelector("ul") as HTMLUListElement;
 const list = new ListTemplates(ul);
 
+let values: [string, string, number];
+values = [tofrom.value, details.value, amount.valueAsNumber];
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   let doc: HasFormatter;
 
   if (type.value === "invoice") {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, "start");
 });
-
-// GENERICS
-
-const addUID = <T extends { name: string }>(obj: T) => {
-  //   console.log("obj", obj);
-  let uid = Math.floor(Math.random() * 100);
-  return { ...obj, uid };
-};
-
-// const addUID = <T extends object>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return {...obj, uid};
-// }
-
-// const addUID = <T extends { name: string }>(obj: T) => {
-//   let uid = Math.floor(Math.random() * 100);
-//   return { ...obj, uid };
-// };
-
-let docOne = addUID({ name: "pedro", age: 40 });
-// let docTwo = addUID("shaun");
-
-console.log(docOne.name);
-
-// with interfaces
-interface Resource<T> {
-  uid: number;
-  resourceName: string;
-  data: T;
-}
-
-const docThree: Resource<object> = {
-  uid: 1,
-  resourceName: "person",
-  data: { name: "shaun" },
-};
-
-const docFour: Resource<string[]> = {
-  uid: 1,
-  resourceName: "shoppingList",
-  data: ["bread", "milk"],
-};
-
-console.log(docThree, docFour);
